66996f9cdd21885c5d3867cb90c7ab17a9c8d9b7
zhangyanfang-gzmu <83931049+zhangyanfang-gzmu@users.noreply.github.com>
2021-05-18 16:06:26 +0800

    Update Test01.java

--- javademo/src/com/zyf/javademo/Test01.java
+++ javademo/src/com/zyf/javademo/Test01.java
@@ -4,5 +4,6 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("李鹏是猪");
         System.out.println("修改了文件");
+        System.out.println("pull");
     }
 }

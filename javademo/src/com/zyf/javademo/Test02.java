50d4e073558b3d1ba02c8416a2566f1160ed7cf2
ZYF_gzmu <1424270288@qq.com>
2021-05-18 16:11:56 +0800

    提交修改后的Test02

--- javademo/src/com/zyf/javademo/Test02.java
+++ javademo/src/com/zyf/javademo/Test02.java
@@ -1,4 +1,9 @@
 package com.zyf.javademo;
 
 public class Test02 {
+    public static void main(String[] args) {
+        System.out.println("李鹏是猪");
+        System.out.println("修改了文件");
+        System.out.println("pull");
+    }
 }
